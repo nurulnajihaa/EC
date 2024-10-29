79616dbf91db1bbdfaa151a25b2049bd96b0e3fb
nurulnajihaa <94379680+nurulnajihaa@users.noreply.github.com>
2024-10-29 13:48:37 +0800

    Update trystreamlit.py

--- trystreamlit.py
+++ trystreamlit.py
@@ -14,8 +14,8 @@ POP_SIZE = 500
 MUT_RATE = 0.2
 
 #TARGET: Our goal.
-#TARGET = 'Nurul'
-TARGET = st.text_input("Enter your name")
+TARGET = 'Nurul'
+#TARGET = st.text_input("Enter your name")
 
 #GENES: Options from which our population would be created.
 GENES = ' abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
