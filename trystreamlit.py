1b88c96e4f169cbf64541b79bcdf3e84abb8dea9
nurulnajihaa <94379680+nurulnajihaa@users.noreply.github.com>
2024-11-03 20:53:42 +0800

    Update buang no rate lepas number input

--- trystreamlit.py
+++ trystreamlit.py
@@ -16,7 +16,7 @@ MUT_RATE = 0.2
 #TARGET: Our goal.
 #TARGET = 'Nurul'
 TARGET = st.text_input("Enter your name","Nurul")
-TARGET = st.number_input("Enter your mutation rate","0.10")
+TARGET = st.number_input("Enter your mutation rate")
 
 #GENES: Options from which our population would be created.
 GENES = ' abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
