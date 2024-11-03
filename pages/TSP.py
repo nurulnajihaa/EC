ade71914d5d290acdfdfd987224dd50ba3cbfe0f
nurulnajihaa <94379680+nurulnajihaa@users.noreply.github.com>
2024-11-03 22:59:24 +0800

    Update delete theme

--- pages/TSP.py
+++ pages/TSP.py
@@ -8,10 +8,6 @@ import statistics
 import pandas as pd
 import seaborn as sns
 
-[theme]
-base="dark"
-primaryColor="purple"
-
 st.set_page_config(page_title="Traveling Salesman Problem using Genetic Algorithm")
 st.header("Traveling Salesman", divider="gray")
 
